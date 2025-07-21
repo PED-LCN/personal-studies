27a38d8bc0c4370b03224c919823447b2a124fc1
P#SHAD#L <ppdro19@gmail.com>
2025-07-21 00:44:53 -0300

    entendendo variaveis e desafio de variaveis em java

---
+++ java/CursoJava/src/Fundamentos/AreaCircumference.java
@@ -0,0 +1,18 @@
+package Fundamentos;
+
+public class AreaCircumference {
+    public static void main(String[] args) {
+        double RAIO = 3.4;
+        final double PI = 3.14159;
+        double AREA = PI * RAIO * RAIO;
+
+        System.out.println(AREA);
+
+        RAIO = 10;
+        AREA = PI * RAIO * RAIO;
+
+        System.out.println("Area = "+ AREA+"M²");
+
+
+    }
+}
---
+++ java/CursoJava/src/Fundamentos/Temperature.java
@@ -0,0 +1,19 @@
+package Fundamentos;
+
+public class Temperature {
+    public static void main(String[] args) {
+        //°C = (°F-32) * 5/9
+        final double AJUSTE = 32;
+        final double FATOR = 5.0/9.0;
+        double F = 86 ;
+
+        double C = (F - AJUSTE) * FATOR;
+        System.out.println("Se a temperatura em fahrenheit for "+F+"°F");
+        System.out.println("A temperatura será de "+C+"°C");
+         F = 150 ;
+
+         C = (F - AJUSTE) * FATOR;
+        System.out.println("Se a temperatura em fahrenheit for "+F+"°F");
+        System.out.println("A temperatura será de "+C+"°C");
+    }
+}
