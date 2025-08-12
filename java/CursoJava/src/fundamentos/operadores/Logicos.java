1278a03822d8c08d3cdf78d410d1a1918ffb4a44
P#SHAD#L <ppdro19@gmail.com>
2025-08-12 15:29:52 -0300

    logic operators

--- java/CursoJava/src/fundamentos/operadores/Logicos.java
+++ java/CursoJava/src/fundamentos/operadores/Logicos.java
@@ -1,4 +1,36 @@
 package fundamentos.operadores;
 
 public class Logicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = 3 > 7;
+        System.out.println(condicao1 && !condicao2);
+        System.out.println(condicao1 || condicao2);
+        System.out.println(condicao1 ^ condicao2);
+        System.out.println(!!condicao1);
+        System.out.println(!condicao2);
+
+        System.out.println("Tabela-verdade do E");
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+
+        System.out.println("Tabela-verdade do Ou (or)");
+        System.out.println(true  || true);
+        System.out.println(true  || false);
+        System.out.println(false ||  true);
+        System.out.println(false ||  false);
+
+        System.out.println("Tabela-verdade do Xor");
+        System.out.println(true  ^ true);
+        System.out.println(true  ^ false);
+        System.out.println(false ^  true);
+        System.out.println(false ^  false);
+
+        System.out.println("Tabela-verdade do Not");
+        System.out.println(!true);
+        System.out.println(!false);
+
+    }
 }
