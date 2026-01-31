a04d93f3f36e86d3580877d93aed34146afe8957
PED-LCN <ppdro19@gmail.com>
2026-01-30 23:27:34 -0300

    fix flies

--- java/CursoJava/src/oo/heranca/desafio/Carro.java
+++ java/CursoJava/src/oo/heranca/desafio/Carro.java
@@ -2,10 +2,19 @@ package oo.heranca.desafio;
 
 public class Carro {
 
+    final int VELOCIDADE_MAXIMA;
     int velocidadeAtual;
+    int delta= 5;
+
+    Carro(int VELOCIDADE_MAXIMA) {
+        this.VELOCIDADE_MAXIMA = VELOCIDADE_MAXIMA;
+    }
 
     void Acelerar(){
-        velocidadeAtual += 5;
+        if (velocidadeAtual + delta > VELOCIDADE_MAXIMA){
+            velocidadeAtual = VELOCIDADE_MAXIMA;
+        }
+        else velocidadeAtual += delta;
     }
 
     void Freiar(){
--- java/CursoJava/src/oo/heranca/desafio/Ferrari.java
+++ java/CursoJava/src/oo/heranca/desafio/Ferrari.java
@@ -2,6 +2,10 @@ package oo.heranca.desafio;
 
 public class Ferrari extends Carro {
 
+    Ferrari(){
+        super(320);
+    }
+
     @Override
     void Acelerar() {
         velocidadeAtual += 15;
